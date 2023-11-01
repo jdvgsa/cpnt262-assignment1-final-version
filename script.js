592d3cb9a305f277aa1e9fcb2d0f5509f73b7723
jdvgsa <johnr.dagsa@outlook.com>
2023-10-31 20:56:25 -0600

    created logic for darkmode

--- script.js
+++ script.js
@@ -13,3 +13,23 @@ toggleButton.addEventListener("click", function() {
         toggleParagraph.classList.add("hidden");
     }
 });
+
+// Dark Mode Toggle
+
+const darkMode = document.getElementById("darkModeToggle")
+const htmlBody = document.body
+
+/* Button should initially say Toggle Dark Mode, when clicked Text on the button should change to
+   "Toggle Light Mode" as well as change the background and text color accordingly. */
+
+darkMode.addEventListener("click", function() {
+    if (htmlBody.classList.contains("darkBackground")){
+      htmlBody.classList.remove("darkBackground");
+      darkMode.innerHTML = "Toggle Dark Mode";
+  }
+  else {
+    htmlBody.classList.add("darkBackground");
+    darkMode.innerHTML = "Toggle Light Mode";
+  }
+
+});
