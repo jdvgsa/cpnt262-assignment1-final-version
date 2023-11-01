8b83889b351dc1c8b6d6dffb065d602bfd56f419
jdvgsa <johnr.dagsa@outlook.com>
2023-10-31 21:08:40 -0600

    fixed toggle logic for hidden text

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// Read more button Toggle
+// Read more Toggle
 
 const toggleButton = document.getElementById("toggleButton");
 const toggleParagraph = document.getElementById("toggleParagraph");
 
-/* Button should display hidden text when clicked. Normal state is for the paragraph to be hidden,
-   however if the reader chooses to decide to read more, extra text will be displayed. */
+/* Extra text normal state is hidden, but when toggled by the button it will display
+   the extra hidden text. Button Text will also change from "Read More" to "Read Less" and vice versa. */
 
 toggleButton.addEventListener("click", function() {
     if (toggleParagraph.classList.contains("hidden")) {
         toggleParagraph.classList.remove("hidden");
+        toggleButton.innerHTML = "Read Less";
     } else {
         toggleParagraph.classList.add("hidden");
+        toggleButton.innerHTML = "Read More";
     }
 });
 
